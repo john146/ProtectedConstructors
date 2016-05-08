d749e7c7b6b60cd01c6dcec333e99263e5dd5a9d
John Ahrens <john@john-ahrens.com>
2016-05-08 10:56:12 -0700

    Initial Commit

---
+++ ProtectedConstructors/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ProtectedConstructors
+//
+//  Created by John Ahrens on 5/8/16.
+//  Copyright © 2016 John Ahrens. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
